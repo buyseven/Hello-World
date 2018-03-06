462fa6390d2ca9665b85137b74f6a08bdd340af8
baiqi <baiqi54@163.com>
2018-03-06 22:25:21 +0800

    '2-8~2-10'

---
+++ favourite_num.py
@@ -0,0 +1,4 @@
+#coding=gbk
+favourite_num = 9 #把9存储到变量favourite_num中
+message = " my favourite number is " + str(favourite_num) + " ,it is the largest sigle number" #使用变量创建一条消息
+print(message) #打印这条消息
