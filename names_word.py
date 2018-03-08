f0f51c17a997087f2e6c912f885d625d54d6aafb
baiqi <baiqi54@163.com>
2018-03-08 21:58:45 +0800

    '3-2'

---
+++ names_word.py
@@ -0,0 +1,8 @@
+#3-2
+names = ['zh','xz','xq']
+message1 = names[0] + ',how are you?'
+message2 = names[1] + ',how are you?'
+message3 = names[2] + ',how are you?'
+print(message1)
+print(message2)
+print(message3)
