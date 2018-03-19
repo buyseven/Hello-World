403b2d8b6c76f226da477fd2ae7815594255aaf9
baiqi <baiqi54@163.com>
2018-03-19 22:02:31 +0800

    '3-3'

---
+++ commuter_way.py
@@ -0,0 +1,4 @@
+commuter_way = ['motorcycle','car','train']
+print('I would like to own a Honda ' + commuter_way[0] + '.')
+print('I would like to own a  bmw ' + commuter_way[1] + '.')
+print('I like travelling by ' + commuter_way[2] + '.')
